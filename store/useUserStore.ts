36f6e1b16ded1a2f4a8fc085b3dc3c5f8d59da9d
Hansjason97 <hansadiogo@gmail.com>
2025-05-31 09:14:50 +0100

    added hydration to prevent bad redirection on refresh

--- store/useUserStore.ts
+++ store/useUserStore.ts
@@ -4,6 +4,7 @@ import { createJSONStorage, persist } from "zustand/middleware";
 
 interface store {
     user?: User;
+    isHydrated: boolean;
     login: (data: User) => void;
     logout: () => void;
 }
@@ -12,12 +13,18 @@ const useStore = create<store>()(
     persist(
         (set, get)=> ({
             user: undefined,
+            isHydrated: false,
             login: (data: User)=>set({user: data}),
             logout: ()=>set({user: undefined})
         }),
         {
             name: "creapp",
             storage: createJSONStorage(()=> sessionStorage),
+            onRehydrateStorage: (state)=>{
+                if (state) {
+                    state.isHydrated = true;
+                }
+            }
         }
     )
 )
