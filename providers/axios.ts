f55d7d4404c28a1ebfb8f5ee7c45ddc14545f2f1
wintercode <kenfackjordanjunior@gmail.com>
2025-10-30 10:47:37 +0100

    fix: update baseURL to use secure API endpoint

--- providers/axios.ts
+++ providers/axios.ts
@@ -3,7 +3,7 @@ import axios, { AxiosError, AxiosResponse } from "axios";
 import { toast } from "sonner";
 
 const baseURL =
-  process.env.NEXT_PUBLIC_API || "http://localhost:5000/api/v1.0.0";
+  process.env.NEXT_PUBLIC_API || "https://creappapi.krestdev.com/api/v1.0.0";
 
 const api = axios.create({
   baseURL,
