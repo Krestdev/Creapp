93d118f2ac42df433e4f9c4b27ebc8b077fae8d2
socrateNz <sssnzogning0@gmail.com>
2026-01-16 08:27:18 +0100

    feat: Create a new sales orders dashboard page.

--- app/tableau-de-bord/(sales)/commande/page.tsx
+++ app/tableau-de-bord/(sales)/commande/page.tsx
@@ -1,53 +1,9 @@
 "use client";
 
-import Bdcommande from "@/components/bdcommande/bdcommande";
-import PageTitle from "@/components/pageTitle";
-import { Button } from "@/components/ui/button";
-import { useStore } from "@/providers/datastore";
-import Link from "next/link";
-
 const Page = () => {
-  const { user } = useStore();
-  const links = [
-    {
-      title: "Demande de cotation",
-      href: "./commande/cotation",
-    },
-    { title: "Devis", href: "./commande/devis" },
-    {
-      title: "Bons de commande",
-      href: "./commande/bon-de-commande",
-    },
-    { title: "Paiements", href: "/tableau-de-bord/bdcommande/paiements" },
-  ];
   return (
     <div className="flex flex-col gap-6">
-      {/* page title */}
-      <PageTitle
-        title="Bons de commande"
-        subtitle="Consultez et gérez les éléments relatifs aux bons de commande"
-        color="red"
-      >
-        {links
-          .filter(
-            (x) =>
-              !(
-                x.title === "Approbation" &&
-                !user?.role.flatMap((r) => r.label).includes("MANAGER")
-              )
-          )
-          .map((link, id) => {
-            const isLast = links.length > 1 ? id === links.length - 1 : false;
-            return (
-              <Link key={id} href={link.href}>
-                <Button size={"lg"} variant={isLast ? "accent" : "ghost"}>
-                  {link.title}
-                </Button>
-              </Link>
-            );
-          })}
-      </PageTitle>
-      <Bdcommande />
+
     </div>
   );
 };
