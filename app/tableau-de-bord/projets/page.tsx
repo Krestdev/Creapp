06b634b927bb1ddf1aa89cf1c4471eddc9473892
socrateNz <sssnzogning0@gmail.com>
2026-01-19 19:00:24 +0100

    projet

--- app/tableau-de-bord/projets/page.tsx
+++ app/tableau-de-bord/projets/page.tsx
@@ -1,6 +1,17 @@
 import PageTitle from "@/components/pageTitle";
+import ProjectListPage from "@/components/projets/liste";
+import { Button } from "@/components/ui/button";
+import { cn } from "@/lib/utils";
+import Link from "next/link";
 
 function Page() {
+
+  const links = [
+    {
+      href: "/tableau-de-bord/projets/creer",
+      label: "Créer un projet",
+    },
+  ];
   return (
     <div className="flex flex-col gap-6">
       {/* page title */}
@@ -8,8 +19,27 @@ function Page() {
         title="Projets"
         subtitle="Consultez et gérez les projets."
         color="red"
-      />
-      {/* <ProjectsPage /> */}
+      >
+        {links
+          .map((link, id) => {
+            const isLast = links.length > 1 ? id === links.length - 1 : false;
+            return (
+              <Link
+                key={id}
+                href={link.href}
+                className={cn("cursor-not-allowed")}
+              >
+                <Button
+                  size={"lg"}
+                  variant={isLast ? "accent" : "ghost"}
+                >
+                  {link.label}
+                </Button>
+              </Link>
+            );
+          })}
+      </PageTitle>
+      <ProjectListPage />
     </div>
   );
 }
