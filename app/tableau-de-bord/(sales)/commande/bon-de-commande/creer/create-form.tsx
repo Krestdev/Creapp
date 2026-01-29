7023d22792cf93cc8cc2db1d3c00baf0cf64171d
Hansjason97 <hansadiogo@gmail.com>
2026-01-29 11:21:48 +0100

    disabled prev date on create bon de commande

--- app/tableau-de-bord/(sales)/commande/bon-de-commande/creer/create-form.tsx
+++ app/tableau-de-bord/(sales)/commande/bon-de-commande/creer/create-form.tsx
@@ -423,6 +423,7 @@ function CreateForm() {
                           field.onChange(value);
                           setSelectDate(false);
                         }}
+                        disabled={(date)=> date < new Date()}
                       />
                     </PopoverContent>
                   </Popover>
