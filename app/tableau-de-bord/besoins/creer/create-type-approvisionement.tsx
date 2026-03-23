4ed64751897d89eb57e242939efbec3df3bdf747
wintercode <kenfackjordanjunior@gmail.com>
2026-03-23 21:44:52 +0100

    approvisionement

---
+++ app/tableau-de-bord/besoins/creer/create-type-approvisionement.tsx
@@ -0,0 +1,309 @@
+"use client";
+import { Button } from "@/components/ui/button";
+import { Calendar } from "@/components/ui/calendar";
+import {
+  Combobox,
+  ComboboxContent,
+  ComboboxEmpty,
+  ComboboxInput,
+  ComboboxItem,
+  ComboboxList,
+} from "@/components/ui/combobox";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from "@/components/ui/popover";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+import { Textarea } from "@/components/ui/textarea";
+import { useStore } from "@/providers/datastore";
+import {
+  newRequestApprovisionement,
+  newRequestGas,
+  requestQ,
+} from "@/queries/requestModule";
+import { Category, PRIORITIES, ProjectT, User, Vehicle } from "@/types/types";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useMutation } from "@tanstack/react-query";
+import { format } from "date-fns";
+import { CalendarIcon } from "lucide-react";
+import { useRouter } from "next/navigation";
+import React from "react";
+import { useForm } from "react-hook-form";
+import { toast } from "sonner";
+import z from "zod";
+
+interface Props {
+  users: Array<User>;
+  categories: Array<Category>;
+  projects: Array<ProjectT>;
+}
+
+const REQUEST_PRIORITIES = PRIORITIES.map((m) => m.value) as [
+  (typeof PRIORITIES)[number]["value"],
+  ...(typeof PRIORITIES)[number]["value"][],
+];
+
+const today = new Date();
+
+const formSchema = z.object({
+  label: z
+    .string({ message: "Veuillez renseigner un titre" })
+    .min(5, { message: "Trop court" })
+    .max(50, { message: "Trop long" }),
+  description: z.string({ message: "Veuillez renseigner une description" }),
+  more: z.string().optional(),
+  categoryId: z.coerce.number({
+    message: "Veuillez sélectionner une catégorie",
+  }),
+  amount: z.coerce.number({ message: "Veuillez renseigner un montant" }),
+  dueDate: z.string({ message: "Veuillez définir une date" }).refine(
+    (val) => {
+      const d = new Date(val);
+      return !isNaN(d.getTime()) && d >= today;
+    },
+    { message: "Date invalide" },
+  ),
+  priority: z.enum(REQUEST_PRIORITIES),
+});
+
+function CreateTypeApprovisionement({ users, categories, projects }: Props) {
+  const { user } = useStore();
+  const router = useRouter();
+
+  const [dueDate, setDueDate] = React.useState<boolean>(false);
+
+  const today = new Date();
+  const defaultDate = new Date();
+  defaultDate.setDate(today.getDate() + 7);
+
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      label: "Aprovisionement",
+      description: "Aprovisionement Pour Carburent et Transport",
+      amount: 100,
+      dueDate: format(defaultDate, "yyyy-MM-dd"),
+      priority: "low",
+      categoryId: undefined,
+      more: "",
+    },
+  });
+
+  const { mutate, isPending } = useMutation({
+    mutationFn: async (payload: newRequestApprovisionement) =>
+      requestQ.createApprovisionement(payload),
+    onSuccess: () => {
+      toast.success("Votre besoin a été soumis avec succès !");
+      router.push("./mes-besoins");
+    },
+    onError: (error: Error) => {
+      toast.error(error.message);
+    },
+  });
+
+  const onSubmit = (values: z.infer<typeof formSchema>) => {
+    const description =
+      values.description === "Mission"
+        ? values.description.concat(" - ", values.more ?? "")
+        : values.description;
+    mutate({
+      label: values.label,
+      description,
+      unit: "FCFA",
+      benef: [user?.id ?? 0],
+      dueDate: new Date(values.dueDate),
+      priority: "medium",
+      categoryId: values.categoryId,
+      quantity: 1,
+      type: "appro",
+    });
+  };
+  console.log(form.formState.errors);
+  return (
+    <Form {...form}>
+      <form onSubmit={form.handleSubmit(onSubmit)} className="form-3xl">
+        <FormField
+          control={form.control}
+          name="label"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel isRequired>{"Titre"}</FormLabel>
+              <FormControl>
+                <Input placeholder="Ex. Carburant" {...field} />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        {/* Category */}
+        <FormField
+          control={form.control}
+          name="categoryId"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel isRequired>{"Categorie"}</FormLabel>
+              <FormControl>
+                <Select
+                  defaultValue={field.value ? String(field.value) : undefined}
+                  onValueChange={field.onChange}
+                >
+                  <SelectTrigger className="min-w-60 w-full">
+                    <SelectValue placeholder="Sélectionner" />
+                  </SelectTrigger>
+                  <SelectContent>
+                    {categories.filter((c) => c.type.type === "appro")
+                      .length === 0 ? (
+                      <SelectItem value="#" disabled>
+                        {"Aucune catégorie enregistrée"}
+                      </SelectItem>
+                    ) : (
+                      categories
+                        .filter((c) => c.type.type === "appro")
+                        .map((category) => (
+                          <SelectItem
+                            key={category.id}
+                            value={category.id.toString()}
+                          >
+                            {category.label}
+                          </SelectItem>
+                        ))
+                    )}
+                  </SelectContent>
+                </Select>
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name="description"
+          render={({ field }) => (
+            <FormItem className="@min-[640px]:col-span-full">
+              <FormLabel isRequired>{"Motif"}</FormLabel>
+              <FormControl>
+                <Input placeholder="Ex. Carburant" {...field} disabled />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        {/* Date limite de soumission */}
+        <FormField
+          control={form.control}
+          name="dueDate"
+          render={({ field }) => {
+            // Convertir la valeur string en Date pour le calendrier
+            const selectedDate = field.value
+              ? new Date(field.value)
+              : undefined;
+
+            return (
+              <FormItem>
+                <FormLabel isRequired>{"Date limite"}</FormLabel>
+                <FormControl>
+                  <div className="relative flex gap-2">
+                    <Input
+                      id={field.name}
+                      value={field.value || ""}
+                      placeholder="Sélectionner une date"
+                      className="bg-background pr-10"
+                      onChange={(e) => {
+                        field.onChange(e.target.value);
+                      }}
+                      onKeyDown={(e) => {
+                        if (e.key === "ArrowDown") {
+                          e.preventDefault();
+                          setDueDate(true);
+                        }
+                      }}
+                    />
+                    <Popover open={dueDate} onOpenChange={setDueDate}>
+                      <PopoverTrigger asChild>
+                        <Button
+                          id="date-picker"
+                          type="button"
+                          variant="ghost"
+                          className="absolute top-1/2 right-2 size-6 -translate-y-1/2"
+                        >
+                          <CalendarIcon className="size-3.5" />
+                          <span className="sr-only">
+                            {"Sélectionner une date"}
+                          </span>
+                        </Button>
+                      </PopoverTrigger>
+                      <PopoverContent
+                        className="w-auto overflow-hidden p-0"
+                        align="end"
+                        alignOffset={-8}
+                        sideOffset={10}
+                      >
+                        <Calendar
+                          mode="single"
+                          selected={selectedDate}
+                          defaultMonth={selectedDate || today}
+                          captionLayout="dropdown"
+                          onSelect={(date) => {
+                            if (!date) return;
+                            const value = format(date, "yyyy-MM-dd");
+                            field.onChange(value);
+                            setDueDate(false);
+                          }}
+                          disabled={(date) => date < new Date()}
+                        />
+                      </PopoverContent>
+                    </Popover>
+                  </div>
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            );
+          }}
+        />
+        {/* Vehicle */}
+        <FormField
+          control={form.control}
+          name="amount"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel isRequired>{"Montant"}</FormLabel>
+              <FormControl>
+                <Input placeholder="Ex. 100" {...field} />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <div className="@min-[640px]:col-span-full w-full flex justify-end">
+          <Button
+            variant={"primary"}
+            type="submit"
+            disabled={isPending}
+            isLoading={isPending}
+            onClick={() => console.log(form.getValues())}
+          >
+            {"Soumettre"}
+          </Button>
+        </div>
+      </form>
+    </Form>
+  );
+}
+
+export default CreateTypeApprovisionement;
