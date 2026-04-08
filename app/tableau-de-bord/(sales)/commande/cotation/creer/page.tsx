771cfa659f98b2833d351c3f96753a0f32b2fed2
socrateNz <sssnzogning0@gmail.com>
2026-04-08 10:19:32 +0100

    select user during cotation creation

--- app/tableau-de-bord/(sales)/commande/cotation/creer/page.tsx
+++ app/tableau-de-bord/(sales)/commande/cotation/creer/page.tsx
@@ -3,6 +3,7 @@ import CreateCotation from "@/components/bdcommande/createCommande";
 import ErrorPage from "@/components/error-page";
 import LoadingPage from "@/components/loading-page";
 import PageTitle from "@/components/pageTitle";
+import { userQ } from "@/queries/baseModule";
 import { categoryQ } from "@/queries/categoryModule";
 import { commandRqstQ } from "@/queries/commandRqstModule";
 import { requestQ } from "@/queries/requestModule";
@@ -16,9 +17,14 @@ const CreerPage = () => {
     },
   });
 
+  const getUsers = useQuery({
+    queryKey: ["users"],
+    queryFn: () => userQ.getAll(),
+  });
+
   const getCommandRequests = useQuery({
     queryKey: ["commands"],
-    queryFn: commandRqstQ.getAll,
+    queryFn: () => commandRqstQ.getAll(),
   });
 
   const getCategories = useQuery({
@@ -31,14 +37,16 @@ const CreerPage = () => {
   if (
     getRequests.isLoading ||
     getCommandRequests.isLoading ||
-    getCategories.isLoading
+    getCategories.isLoading ||
+    getUsers.isLoading
   )
     return <LoadingPage />;
 
   if (
     getRequests.isError ||
     getCommandRequests.isError ||
-    getCategories.isError
+    getCategories.isError ||
+    getUsers.isError
   )
     return (
       <ErrorPage
@@ -46,6 +54,7 @@ const CreerPage = () => {
           getRequests.error ||
           getCommandRequests.error ||
           getCategories.error ||
+          getUsers.error ||
           undefined
         }
       />
@@ -54,8 +63,12 @@ const CreerPage = () => {
   if (
     getRequests.isSuccess &&
     getCategories.isSuccess &&
-    getCommandRequests.isSuccess
-  )
+    getCommandRequests.isSuccess &&
+    getUsers.isSuccess
+  ) {
+
+    console.log(getUsers.data.data);
+    
     return (
       <div className="flex flex-col gap-6">
         <PageTitle
@@ -66,12 +79,14 @@ const CreerPage = () => {
           color={"blue"}
         />
         <CreateCotation
+          users={getUsers.data.data}
           requests={getRequests.data.data}
           quotationRequests={getCommandRequests.data.data}
           categories={getCategories.data.data}
         />
       </div>
     );
+  }
 };
 
 export default CreerPage;
