eada4fd76f6b3d6ffcbb5e0c0bc676d0a0908471
Hansjason97 <hansadiogo@gmail.com>
2026-04-24 16:23:59 +0100

    feat: add CreateProject component with user data fetching and form integration

--- components/projets/createProject.tsx
+++ components/projets/createProject.tsx
@@ -1,16 +1,38 @@
+"use client";
+import { useStore } from "@/providers/datastore";
 import { ProjectCreateForm } from "./create-project";
+import { useQuery } from "@tanstack/react-query";
+import { userQ } from "@/queries/baseModule";
+import LoadingPage from "../loading-page";
+import ErrorPage from "../error-page";
 
 const CreateProject = () => {
-  return (
-    <div className="flex flex-col gap-4">
-      <div className="flex flex-col">
-        <div className="flex justify-between">
-          <h2>Créer un utilisateur</h2>
+  const { user } = useStore();
+  const {
+    data: users,
+    isSuccess,
+    isError,
+    isLoading,
+    error,
+  } = useQuery({
+    queryKey: ["users"],
+    queryFn: () => userQ.getAll(),
+  });
+
+  if (isLoading) return <LoadingPage />;
+  if (isError) return <ErrorPage error={error} />;
+  if (isSuccess && !!user) {
+    return (
+      <div className="flex flex-col gap-4">
+        <div className="flex flex-col">
+          <div className="flex justify-between">
+            <h2>Créer un utilisateur</h2>
+          </div>
+          <ProjectCreateForm users={users.data} userId={user.id} />
         </div>
-        <ProjectCreateForm />
       </div>
-    </div>
-  );
+    );
+  }
 };
 
 export default CreateProject;
