b0bcce6fe53b78d4780f6748a8b36b54c3f84de0
Hansjason97 <hansadiogo@gmail.com>
2026-04-28 14:36:10 +0100

    feat: implement cancel transfer dialog with reason validation and mutation handling

--- app/tableau-de-bord/banques/transactions/transferts/cancel-transfert.tsx
+++ app/tableau-de-bord/banques/transactions/transferts/cancel-transfert.tsx
@@ -8,10 +8,22 @@ import {
   DialogHeader,
   DialogTitle,
 } from "@/components/ui/dialog";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Textarea } from "@/components/ui/textarea";
 import { transactionQ } from "@/queries/transaction";
 import { Transaction } from "@/types/types";
+import { zodResolver } from "@hookform/resolvers/zod";
 import { useMutation } from "@tanstack/react-query";
+import { useForm } from "react-hook-form";
 import { toast } from "sonner";
+import z from "zod";
 
 interface CancelProps {
   open: boolean;
@@ -19,10 +31,23 @@ interface CancelProps {
   transfer: Transaction;
 }
 
+const formSchema = z.object({
+  reason: z.string().min(5, "La raison doit contenir au moins 5 caractere"),
+});
+
+type FormSchemaType = z.infer<typeof formSchema>;
+
 export function CancelTransfert({ open, onOpenChange, transfer }: CancelProps) {
+  const form = useForm<FormSchemaType>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      reason: "",
+    },
+  });
+
   // Mutation pour mettre à jour le transfert
   const cancelMutation = useMutation({
-    mutationFn: async () => {
+    mutationFn: async (data: FormSchemaType) => {
       return transactionQ.cancel(transfer.id);
     },
     onSuccess: () => {
@@ -34,6 +59,10 @@ export function CancelTransfert({ open, onOpenChange, transfer }: CancelProps) {
     },
   });
 
+  function onSubmit(data: FormSchemaType) {
+    cancelMutation.mutate(data);
+  }
+
   return (
     <Dialog open={open} onOpenChange={onOpenChange}>
       <DialogContent>
@@ -44,26 +73,43 @@ export function CancelTransfert({ open, onOpenChange, transfer }: CancelProps) {
             Voulez-vous continuer ?
           </DialogDescription>
         </DialogHeader>
-        {/* Footer avec boutons - Style cohérent */}
-        <DialogFooter>
-          <Button
-            type="button"
-            variant="destructive"
-            disabled={cancelMutation.isPending}
-            isLoading={cancelMutation.isPending}
-            onClick={() => cancelMutation.mutate()}
-          >
-            {cancelMutation.isPending ? "Annulation..." : "Oui, annuler"}
-          </Button>
-          <Button
-            type="button"
-            variant="outline"
-            onClick={() => onOpenChange(false)}
-            disabled={cancelMutation.isPending}
-          >
-            Non
-          </Button>
-        </DialogFooter>
+        <Form {...form}>
+          <form onSubmit={form.handleSubmit(onSubmit)} className="grid gap-3">
+            <FormField
+              control={form.control}
+              name="reason"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel isRequired>Motif</FormLabel>
+                  <FormControl>
+                    <Textarea {...field} placeholder="Motif de l'annulation" />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <DialogFooter>
+              <Button
+                type="submit"
+                variant="destructive"
+                disabled={cancelMutation.isPending}
+                isLoading={cancelMutation.isPending}
+              >
+                {cancelMutation.isPending ? "Annulation..." : "Oui, annuler"}
+              </Button>
+              <Button
+                variant="outline"
+                onClick={(e) => {
+                  e.preventDefault();
+                  onOpenChange(false);
+                }}
+                disabled={cancelMutation.isPending}
+              >
+                Non
+              </Button>
+            </DialogFooter>
+          </form>
+        </Form>
       </DialogContent>
     </Dialog>
   );
