919b0308c18699e9ada5f4e002c0500000112cee
Hansjason97 <hansadiogo@gmail.com>
2026-09-22 14:52:22 +0100

    feat: add transaction query service, transfer management pages, and devis modal component

--- queries/transaction.ts
+++ queries/transaction.ts
@@ -27,6 +27,8 @@ export interface TransactionApprovalParams {
   pageSize?: number | undefined;
   tab: "PENDING" | "COMPLETED";
   bankId?: number | undefined;
+  fromBankId?: number | undefined;
+  toBankId?: number | undefined;
   from?: Date | string | undefined;
   to?: Date | string | undefined;
   amountMin?: number | undefined;
